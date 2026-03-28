bac8d16cd5e125679af554395d89d099e8856f70
shruthika <shruthiruth21@gmail.com>
2026-03-28 12:21:34 +0530

    add , edi ,delete product

--- directbuy-app/src/features/products/productsApi.js
+++ directbuy-app/src/features/products/productsApi.js
@@ -11,8 +11,31 @@ export const productsApi = baseApi.injectEndpoints({
         getProductById: builder.query({
             query: (id) => `product/${id}`,
             providesTags: (result, error, id) => [{ type: 'product', id }]
+        }),
+        deleteProductById: builder.mutation({
+            query: (id) => ({
+                url: `product/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['product']
+        }),
+        addProduct: builder.mutation({
+            query: (newProduct) => ({
+                url: '/product',
+                method: 'POST',
+                body: newProduct
+            }),
+            invalidatesTags: ['product']
+        }),
+        updateProduct: builder.mutation({
+            query: ({ id, product }) => ({
+                url: `product/${id}`,
+                method: 'PUT',
+                body: product
+            }),
+            invalidatesTags: ['product']
         })
     })
 })
 
-export const { useGetProductsQuery, useGetProductByIdQuery } = productsApi
+export const { useGetProductsQuery, useGetProductByIdQuery ,useAddProductMutation,useDeleteProductByIdMutation,useUpdateProductMutation} = productsApi
--- directbuy-app/src/pages/AdminDb.jsx
+++ directbuy-app/src/pages/AdminDb.jsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom"
 import { useAuth } from "../app/AuthProvider"
 
 function AdminDb() {
 
-    const {data,isLoading} = useAuth()
+    const { data, isLoading } = useAuth()
 
-    const user= data?.data
-    if(isLoading) return <h4>Loading...</h4>
+    const user = data?.data
+    if (isLoading) return <h4>Loading...</h4>
     return (
         <div>
-            <h1>Admin Dashboard</h1>    
+            <h1>Admin Dashboard</h1>
             <p>Welcome to the Admin Dashboard. {user?.firstName}</p>
+            <br></br>
+
+            <Link to='/products/add'>Add New Product Link</Link>
         </div>
     )
-}   
+}
 export default AdminDb
